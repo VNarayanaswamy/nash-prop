80f3c933909706e47e9866f621b4f6b44fcd52dd
davidcearl <davidcearl@gmail.com>
2017-10-04 14:36:42 -0500

    update scraper

---
+++ scraper/json_to_db.R
@@ -0,0 +1,86 @@
+# ------------------------------------------------------------------------------
+dfs_from_json <- function(json) {
+  df_list <- jsonlite::fromJSON(json)
+  prop_df <- cbind(df_list$Property, df_list$Building)
+  uuid <- uuid::UUIDgenerate()
+  prop_df$id <- uuid
+  df_list$Sales$id <- uuid
+  df_list$Appraisals$id <- uuid
+  return(list(prop = prop_df,
+              sales = df_list$Sales,
+              appr = df_list$Appraisals))
+}
+
+combine_props <- function(dfs_list) {
+  props <- lapply(dfs_list, '[[', 1)
+  return(jsonlite::rbind_pages(props))
+}
+
+combine_sales <- function(dfs_list) {
+  props <- lapply(dfs_list, '[[', 2)
+  return(jsonlite::rbind_pages(props))
+}
+
+combine_apprs <- function(dfs_list) {
+  props <- lapply(dfs_list, '[[', 3)
+  return(jsonlite::rbind_pages(props))
+}
+
+simple_colnames <- function(df) {
+  df_names <- names(df)
+  df_names <- stringr::str_replace_all(df_names, ' & | |\\*', '_')
+  df_names <- stringr::str_to_lower(df_names)
+  return(df_names)
+}
+
+mutate_props <- function(df) {
+  names(df) <- simple_colnames(df)
+  df <- dplyr::mutate_each(df, 
+                           dplyr::funs(stringr::str_replace_all(., 
+                                                                '\\$|,', '')),
+                           c(10, 13:16, 23))
+  
+  df <- dplyr::mutate_each(df,
+                           dplyr::funs(stringr::str_replace_all(.,
+                                                                ' Acres', '')),
+                           c(20))
+  
+  df <- dplyr::mutate_each(df,
+                           dplyr::funs(as.numeric),
+                           c(10, 13:16, 20, 23, 28:32, 35))
+  
+  df$sale_date <- as.Date(df$sale_date, "%m/%d/%Y")
+  df <- dplyr::mutate(df, zip_code = stringr::str_sub(mailing_address, -5))
+  return(df)
+}
+
+mutate_sales <- function(df) {
+  names(df) <- simple_colnames(df)
+  df$sale_date <- as.Date(df$sale_date, "%m/%d/%Y")
+  df$sale_price <- stringr::str_replace_all(df$sale_price, '\\$|,', '')
+  df$sale_price <- as.numeric(df$sale_price)
+  return(df)
+}
+
+mutate_apprs <- function(df) {
+  df <- dplyr::mutate_each(df,
+                           dplyr::funs(stringr::str_replace_all(.,
+                                                                '\\$|,', '')),
+                           c(3:5,7))
+  
+  df <- dplyr::mutate_each(df, dplyr::funs(as.numeric), c(3:5,7))
+  return(df)
+}
+
+write_db <- function(con, sql_tbl, df){
+  RPostgreSQL::dbWriteTable(con, sql_tbl, df, row.names=F, append=T)
+}
+
+# ------------------------------------------------------------------------------
+
+
+
+
+
+
+
--- scraper/prop_scraper.R
+++ scraper/prop_scraper.R
@@ -1,60 +1,86 @@
-library(stringr)
-library(RCurl)
-library(XML)
-library(jsonlite)
+# ------------------------------------------------------------------------------
+# get basic property info
 
-#takes a range of numbers and returns a nested json of property records
-#each property has three associated pages
-#record types of interest are located by html tags 
-get_prop_rec <- function(num_val) {
-  prop_records <- c()
-  for (prop_num in num_val) {
-    
-    # get prop summary
-    address <- paste0("http://www.padctn.org/prc/property/", prop_num, "/card/1")
-    property_search <- getURL(address)
-    search_page <- htmlParse(property_search)
-    t_page <- paste(capture.output(search_page, file = NULL), collapse = "\n")
-    lat_long <- unlist(str_split(str_sub(unlist(str_extract_all(t_page, '/lat.*>')), 6, 33), '/lng/'))
-    lat <- paste('LAT:', lat_long[1])
-    long <- paste('LNG:', lat_long[2])
-    prop_sum <- str_extract_all(t_page, '<strong>.*</li>')
-    sum_vec <- unlist(prop_sum)
-    tag_rem_sum <- str_trim(str_replace_all(sum_vec, "<.*?>", ""))
-    prop_summary <- toJSON(c(tag_rem_sum, lat, long))
-    
-    # get prop details
-    address <- paste0("http://www.padctn.org/prc/property/", prop_num, "/card/1/interior")
-    property_details <- getURL(address)
-    details_page <- htmlParse(property_details)
-    t_details <- paste(capture.output(details_page, file = NULL), collapse = "\n")
-    juciy_deats <- str_extract_all(t_details, '<strong>.*</li>')
-    deats_vec <- unlist(juciy_deats)
-    tag_rem_deats <- str_trim(str_replace_all(deats_vec, "<.*?>", ""))
-    details_summary <- toJSON(tag_rem_deats)
-        
-    # get prop history
-    address <- paste0("http://www.padctn.org/prc/property/", prop_num, "/card/1/historical")
-    property_history <- getURL(address)
-    history_page <- htmlParse(property_history)
-    t_history <- paste(capture.output(history_page, file = NULL), collapse = "\n")
-    prop_hist  <- str_extract_all(t_history, '<table|<th>.*</th>|<td>.*</td>')
-    hist_vec <- unlist(prop_hist)
-    tag_rem_hist <- str_trim(str_replace_all(hist_vec, "<.*?>", ""))
-    table_starts <- which(match(tag_rem_hist, "<table") == 1)
-    sales_hist <- toJSON(t(matrix(tag_rem_hist[(table_starts[1]+1):(table_starts[2]-1)], nrow = 4)))
-    app_hist <- toJSON(t(matrix(tag_rem_hist[(table_starts[2]+1):length(tag_rem_hist)], nrow = 7)))
-    
-    # combine into single json obj
-    property_record <- toJSON(list(prop_summary, details_summary, sales_hist, app_hist))
-    prop_records <- c(prop_records, property_record)
-    
-  }
-  return(prop_records)
+get_li <- function(parsed_card) {
+  li <- XML::readHTMLList(parsed_card)
+  return(unlist(li[11:15]))
 }
 
+# function for turning li to dataframe
+li_to_df <- function(li) {
+  li <-stringr::str_split(li, ': ', simplify = T)
+  df <- data.frame(matrix(NA_character_, nrow = 1, ncol = dim(li)[1]),
+                   stringsAsFactors = F)
+  names(df) <- li[,1]
+  df[1,] <- li[,2]
+  return(df)
+}
 
+get_lat_lng <- function(parsed_card) {
+  lat_lng_href <- unlist(
+    XML::xpathApply(parsed_card,
+                    "//a[@data-maptype]", 
+                    XML::xmlGetAttr, 
+                    "href"))
+  lat <- stringr::str_sub(lat_lng_href, -26, -16)
+  lng <- stringr::str_sub(lat_lng_href, -12, -1)
+  return(c('lat' = lat, 'lng' = lng))
+}
 
+append_lat_lng <- function(df, lat_lng) {
+  df$lat <- lat_lng[['lat']]
+  df$lng <- lat_lng[['lng']]
+  return(df)
+}
 
+parse_base_card <- function(url) {
+  if (RCurl::url.exists(url)) {
+    parsed_card <- XML::htmlParse(RCurl::getURL(url))
+    df <- li_to_df(get_li(parsed_card))
+    lat_lng <- get_lat_lng(parsed_card)
+    df <- append_lat_lng(df, lat_lng)
+    return(df)
+  }
+}
 
+# ------------------------------------------------------------------------------
+# get interior details
 
+get_interior_li <- function(parsed_card) {
+  li <- XML::readHTMLList(parsed_card)
+  return(unlist(li[11:14]))
+}
+
+parse_interior_card <- function(url) {
+  if (RCurl::url.exists(url)) {
+    parsed_card <- XML::htmlParse(RCurl::getURL(url))
+    df <- li_to_df(get_interior_li(parsed_card))
+    return(df)
+  }
+}
+
+# ------------------------------------------------------------------------------
+# get property history
+
+parse_historical_tbls <- function(url) {
+  if (RCurl::url.exists(url)) {
+    parsed_card <- XML::htmlParse(RCurl::getURL(url))
+    tbls <- XML::readHTMLTable(parsed_card)
+    return(tbls)
+  }
+}
+
+# ------------------------------------------------------------------------------
+# main function
+
+scrape_property <- function(prop_num) {
+  url <- paste0("http://www.padctn.org/prc/property/", prop_num, "/card/1")
+  basic <- parse_base_card(url)
+  interior <- parse_interior_card(paste0(url, "/interior"))
+  hist_tbls <- parse_historical_tbls(paste0(url, "/historical"))
+  prop_data <- list(Property = basic,
+                    Building = interior, 
+                    Sales = hist_tbls[[1]], 
+                    Appraisals = hist_tbls[[2]])
+  return(jsonlite::toJSON(prop_dat))
+}
